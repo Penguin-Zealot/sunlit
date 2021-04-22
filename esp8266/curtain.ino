18af5b90c79ec4b7d76b5f61cca9559ccafc77f7
Yohan <gentoopenguin.zealot@gmail.com>
2021-04-22 13:19:53 +1000

    next js stub

---
+++ esp8266/curtain.ino
@@ -0,0 +1,14 @@
+#include <SocketIoClient.h>
+
+static esp8266::polledTimeout::periodicMs showTimeNow(60000);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+}
